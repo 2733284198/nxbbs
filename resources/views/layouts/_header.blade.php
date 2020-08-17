f8de1324141b85913dea9e0743b287027ae81350
shebaoting <th9th@th9th.com>
2020-08-17 17:44:58 +0800

    分类模型和数据

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <!-- Branding Image -->
         <a class="navbar-brand " href="{{ url('/') }}">
-        LaraBBS
+        NXBBS
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -22,13 +22,17 @@
             <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
             @else
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
-                    {{ Auth::user()->name }}
+                <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle avatar">
+                    <div class="userinfo">
+                        {{ Auth::user()->name }}
+                        <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </div>
+
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
-                    <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+                    <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">我的主页</a>
+                    <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">设置中心</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" id="logout" href="#">
                       <form action="{{ route('logout') }}" method="POST">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,33 +6,42 @@
 
 <div class="row">
 
-  <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
-    <div class="card ">
-      <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
-      <div class="card-body">
-        <h5><strong>个人简介</strong></h5>
-        <p>{{ $user->introduction }}</p>
-        <hr>
-        <h5><strong>注册于</strong></h5>
-        <p>{{ $user->created_at->diffForHumans() }}</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-    <div class="card ">
-      <div class="card-body">
-          <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
-      </div>
-    </div>
-    <hr>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+        {{-- 用户发布的内容 --}}
+        <div class="card ">
+            <div class="card-body">
+                暂无数据 ~_~
+            </div>
+        </div>
 
-    {{-- 用户发布的内容 --}}
-    <div class="card ">
-      <div class="card-body">
-        暂无数据 ~_~
-      </div>
     </div>
 
-  </div>
+
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+
+
+
+
+        <div class="left-bar card">
+            <div class="user-bg"></div>
+            <div class="user-info">
+                <div class="user-avatar"><img
+                        src="{{ $user->avatar }}"
+                        alt="{{ $user->name }}" class="avatar-img"></div>
+                <div class="user-name">
+                    <span class="username">{{ $user->name }}</span>
+                </div>
+            </div>
+            <div class="user-signature">
+                <span>{{ $user->introduction }}</span>
+                <span class="user_created_at">注册: {{ $user->created_at->diffForHumans() }}</span>
+                <span class="user_email">邮箱: {{ $user->email }}</span>
+            </div>
+        </div>
+
+    </div>
+
+
+
 </div>
 @stop
