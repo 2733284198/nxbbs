0ebd4063461b1e69a5240e6baf50f895eeaf43af
shebaoting <th9th@th9th.com>
2020-08-18 21:45:43 +0800

    消息通知列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,48 +1,68 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
-      <!-- Branding Image -->
-      <a class="navbar-brand " href="{{ url('/') }}">
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
       南相产品交流社区
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-      </button>
+    </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto header_navs">
-        <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
+        <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link"
+            href="{{ route('topics.index') }}">话题</a></li>
       </ul>
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
-          <!-- Authentication Links -->
-          @guest
-          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
-          @else
-          <li class="nav-item dropdown">
-            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="40px" height="40px">
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+        <li class="nav-item dropdown" style="display: inline-block;line-height: 40px;margin-right: 20px;">
+
+
+          <div class="header_ctrls_meta top_notice_box"><a href="{{ route('notifications.index') }}"
+              class="account_inbox_switch js_notice_box_switch notify_target"><i class="icon_inbox notify_target"></i>
+
+
+              @if (Auth::user()->notification_count > 0)
+              <span class="badge badge-pill badge-danger">
+                {{ Auth::user()->notification_count }}
+              </span>
+              @endif
+
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
-                个人中心
-              </a>
-              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
-                编辑资料
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" id="logout" href="{{ route('logout') }}">
-                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
-                  {{ csrf_field() }}
-                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                </form>
-              </a>
-            </div>
-          </li>
-          @endguest
+          </div>
+        </li>
+
+        <li class="nav-item dropdown"></li>
+        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">
+          <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="40px" height="40px">
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
+            个人中心
+          </a>
+          <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
+            编辑资料
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" id="logout" href="{{ route('logout') }}">
+            <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
+              {{ csrf_field() }}
+              <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+            </form>
+          </a>
+        </div>
+        </li>
+        @endguest
       </ul>
-      </div>
+    </div>
   </div>
-  </nav>
+</nav>
