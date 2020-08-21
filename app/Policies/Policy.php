cebd26c30e713a0ff7240f980d08cdeb6cd451e2
shebaoting <th9th@th9th.com>
2020-08-21 11:27:20 +0800

    站点权限部署

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -3,7 +3,7 @@
 namespace App\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Dcat\Admin\Admin;
+
 class Policy
 {
     use HandlesAuthorization;
@@ -16,6 +16,9 @@ class Policy
     public function before($user, $ability)
     {
         // 如果用户拥有管理内容的权限的话，即授权通过
+        if ($user->can('manage_contents')) {
+            return true;
+        }
     }
-	    // }
-	}
+    // }
+}
